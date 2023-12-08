0c9292beeaa58e8d888e2526459862c8ee382c46
Mohammad-Ihmeid <mohammadmaher1999@yahoo.com>
2023-12-08 16:39:45 +0300

    حذف جميع الصور الغير مستخدمة

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -13,13 +13,6 @@ class IconsAssets {
   static const String userIcon = "$iconPath/user.png";
 
   //Home Icons
-  static const String openBook = "$iconPath/open-book.png";
-  static const String waiter = "$iconPath/waiter.png";
-  static const String shoppingCart = "$iconPath/shopping-cart.png";
-  static const String officeBuilding = "$iconPath/office-building.png";
-  static const String multipleUsers = "$iconPath/multiple-users-silhouette.png";
-  static const String luggageCart = "$iconPath/luggage-cart.png";
-  static const String factory = "$iconPath/factory.png";
   static const String calendar = "$iconPath/calendar.png";
   static const String map = "$iconPath/map.png";
   //Json Icons
